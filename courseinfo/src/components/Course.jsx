51244275636b5007c79631129543f0b904661d05
Federico Charra <fcharra@hotmail.com>
2024-02-02 19:40:01 -0300

    2.a.1: courseinfo, step6

---
+++ courseinfo/src/components/Course.jsx
@@ -0,0 +1,24 @@
+const Part = ({name, exercises}) => <p>{name} {exercises}</p>
+
+const Header = ({name}) => <h1>{name}</h1>
+
+const Content = ({parts}) => {
+  return (
+    <>
+      {parts.map(part => 
+        <Part key={part.id} name={part.name} exercises={part.exercises} />
+      )}
+    </>
+  )
+}
+
+const Course = ({course}) => {
+  return (
+    <>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+    </>
+  )
+}
+
+export default Course
